1c0d15d41dda8d3430b10b75b5ee3ec81ea39163
RINNO <rinno.bagus@mii.co.id>
2025-10-30 12:43:51 +0000

    add masking policy dan git integration

--- setup.sql
+++ setup.sql
@@ -130,3 +130,86 @@ call send_email()
 select analyze_fleet_utilization('last_2_month')
 
 
+    CREATE OR REPLACE MASKING POLICY email_mask_policy AS (val STRING) RETURNS STRING ->
+      CASE
+        WHEN CURRENT_ROLE() = 'ANALYST_ROLE' THEN '******' || SUBSTRING(val, LENGTH(val) - 4)
+        WHEN CURRENT_ROLE() = 'ADMIN_ROLE' or CURRENT_ROLE() = 'ACCOUNTADMIN' THEN val
+        ELSE 'MASKED'
+      END;
+  
+    ALTER TABLE customers MODIFY COLUMN email SET MASKING POLICY email_mask_policy;\
+
+    
+    CREATE OR REPLACE MASKING POLICY phone_mask_policy AS (val STRING) RETURNS STRING ->
+      CASE
+        WHEN CURRENT_ROLE() = 'ANALYST_ROLE' THEN '*******' || SUBSTRING(val, LENGTH(val) - 4)
+        WHEN CURRENT_ROLE() = 'ADMIN_ROLE' or CURRENT_ROLE() = 'ACCOUNTADMIN' THEN val
+        ELSE 'MASKED'
+      END;
+ 
+  
+    ALTER TABLE customers MODIFY COLUMN phone SET MASKING POLICY phone_mask_policy;
+
+    
+    CREATE OR REPLACE MASKING POLICY driver_license_mask_policy AS (val STRING) RETURNS STRING ->
+      CASE
+        WHEN CURRENT_ROLE() = 'ANALYST_ROLE' THEN '**********' || SUBSTRING(val, LENGTH(val) - 4)
+        WHEN CURRENT_ROLE() = 'ADMIN_ROLE' or CURRENT_ROLE() = 'ACCOUNTADMIN' THEN val
+        ELSE 'MASKED'
+      END;
+  
+    ALTER TABLE customers MODIFY COLUMN driver_license_number SET MASKING POLICY driver_license_mask_policy;
+
+    create or replace role ANALYST_ROLE;
+    create or replace role ADMIN_ROLE;
+    
+    use role accountadmin;
+    
+    CREATE USER beni
+    PASSWORD = 'SecurePassword123!'
+    DEFAULT_ROLE = 'ADMIN_ROLE'
+    DEFAULT_WAREHOUSE = 'COMPUTE_WH';
+
+    grant role ADMIN_ROLE to user beni;
+    grant usage on database hackathon_snowflake to role ADMIN_ROLE;
+    grant usage on schema TRANSPORTATION to role ADMIN_ROLE;
+    GRANT SELECT ON ALL TABLES IN schema TRANSPORTATION TO ROLE ADMIN_ROLE;
+    GRANT USAGE ON ALL CORTEX SEARCH SERVICES IN schema TRANSPORTATION TO ROLE ADMIN_ROLE;
+    GRANT ALL ON ALL SEMANTIC VIEWS IN schema TRANSPORTATION TO ROLE ADMIN_ROLE;
+    GRANT USAGE ON ALL PROCEDURES IN schema TRANSPORTATION TO ROLE ADMIN_ROLE;
+    GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ADMIN_ROLE;
+    
+    grant usage on database SNOWFLAKE_INTELLIGENCE to role ADMIN_ROLE;
+    grant usage on schema SNOWFLAKE_INTELLIGENCE.AGENTS to role ADMIN_ROLE;
+    GRANT USAGE ON AGENT SNOWFLAKE_INTELLIGENCE.AGENTS.SMARTFLEETMONITORINGAGENT to ROLE ADMIN_ROLE;
+
+    CREATE USER siska
+    PASSWORD = 'SecurePassword123!'
+    DEFAULT_ROLE = 'ANALYST_ROLE'
+    DEFAULT_WAREHOUSE = 'COMPUTE_WH';
+
+    grant role ANALYST_ROLE to user siska;
+    grant usage on database hackathon_snowflake to role ANALYST_ROLE;
+    grant usage on schema TRANSPORTATION to role ANALYST_ROLE;
+    GRANT SELECT ON ALL TABLES IN schema TRANSPORTATION TO ROLE ANALYST_ROLE;
+    GRANT USAGE ON ALL CORTEX SEARCH SERVICES IN schema TRANSPORTATION TO ROLE ANALYST_ROLE;
+    GRANT ALL ON ALL SEMANTIC VIEWS IN schema TRANSPORTATION TO ROLE ANALYST_ROLE;
+    GRANT USAGE ON ALL PROCEDURES IN schema TRANSPORTATION TO ROLE ANALYST_ROLE;
+    GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ANALYST_ROLE;
+    
+    grant usage on database SNOWFLAKE_INTELLIGENCE to role ANALYST_ROLE;
+    grant usage on schema SNOWFLAKE_INTELLIGENCE.AGENTS to role ANALYST_ROLE;
+    GRANT USAGE ON AGENT SNOWFLAKE_INTELLIGENCE.AGENTS.SMARTFLEETMONITORINGAGENT to ROLE ANALYST_ROLE;
+
+    CREATE OR REPLACE API INTEGRATION my_git_api_integration
+    API_PROVIDER = git_https_api
+    API_ALLOWED_PREFIXES = ('https://github.com/dai2-metrodata/')
+    API_USER_AUTHENTICATION = (
+    TYPE = snowflake_github_app
+    )
+    ENABLED = TRUE;
+    
+    
+ 
+
+
